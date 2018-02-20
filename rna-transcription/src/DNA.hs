49e02cd895080ab2c05b9e1e8a6d302a1e192e6e
Kapil Koju <kplx2.0@gmail.com>
2018-02-20 08:00:47 +0545

    rna-transcription impl with mapM

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,10 +1,10 @@
 module DNA (toRNA) where
 
-transcribe x = case x of
-    'G' -> 'C'
-    'C' -> 'G'
-    'T' -> 'A'
-    'A' -> 'U'
-
 toRNA :: String -> Maybe String
-toRNA xs = if (all (`elem` "GCTA") xs) then Just (map transcribe xs) else Nothing
+toRNA = mapM transcribe
+    where transcribe x = case x of
+                'G' -> Just 'C'
+                'C' -> Just 'G'
+                'T' -> Just 'A'
+                'A' -> Just 'U'
+                _  -> Nothing
