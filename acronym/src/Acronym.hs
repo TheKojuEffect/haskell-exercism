f33cf6ab6cd6bbb8811be43d2d472fd9ff240248
Kapil Koju <kplx2.0@gmail.com>
2018-02-22 08:27:47 +0545

    Acronym

---
+++ acronym/src/Acronym.hs
@@ -0,0 +1,17 @@
+module Acronym (abbreviate) where
+
+import Data.List
+import Data.Char
+
+append xs x = xs++[toUpper x]
+
+abbreviate :: String -> String
+abbreviate (x:xs) =  acronym [x] x xs
+    where
+        acronym :: String -> Char -> String -> String
+        acronym result _ [] = result
+        acronym result lastChar (y:ys)
+            | lastChar == ' ' || lastChar == '-' = acronym (append result y) y ys
+            | otherwise = if isUpper y && isLower lastChar
+                            then acronym (append result y) y ys
+                            else acronym result y ys
