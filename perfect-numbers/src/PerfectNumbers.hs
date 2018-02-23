9a82ab5ab62bf0927d16316b76ee351afd140d69
Kapil Koju <kplx2.0@gmail.com>
2018-02-23 10:06:54 +0545

    Perfect Numbers

--- perfect-numbers/src/PerfectNumbers.hs
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -4,22 +4,11 @@ import Data.Maybe
 
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
-factors :: Int -> Maybe [Int]
-factors n
-    | n > 0 = Just [x | x <- [1..n-1], n `mod` x == 0]
-    | otherwise = Nothing
-
-
-aliquotSum :: Int -> Maybe Int
-aliquotSum n
-        | isNothing $ factors n =  Nothing
-        | otherwise = Just $ sum $ fromJust (factors n)
-
-
 classify :: Int -> Maybe Classification
 classify n
-        | isNothing $ aliquotSum n = Nothing
-        | n < (fromJust $ aliquotSum n) = Just Abundant
-        | n == (fromJust $ aliquotSum n) = Just Perfect
-        | n > (fromJust $ aliquotSum n) = Just Deficient
-        | otherwise = Nothing
+      | n < 1 = Nothing
+      | aliquotSum == n = Just Perfect
+      | aliquotSum > n = Just Abundant
+      | aliquotSum < n = Just Deficient
+      where
+        aliquotSum = sum [x | x <- [1..(n `div` 2)], n `mod` x == 0]
