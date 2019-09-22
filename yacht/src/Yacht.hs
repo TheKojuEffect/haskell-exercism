136793e022e1be646d5bf95a43277d921860bdfe
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2019-09-22 14:05:53 -0500

    Refactor Yacht into smaller components

--- yacht/src/Yacht.hs
+++ yacht/src/Yacht.hs
@@ -18,42 +18,51 @@ data Category
   | BigStraight
   | Choice
   | Yacht
-  deriving (Read)
 
 sumOf :: Int -> [Int] -> Int
 sumOf n nums = sum $ filter (== n) nums
 
-yacht :: Category -> [Int] -> Int
-yacht Ones dices = sumOf 1 dices
-yacht Twos dices = sumOf 2 dices
-yacht Threes dices = sumOf 3 dices
-yacht Fours dices = sumOf 4 dices
-yacht Fives dices = sumOf 5 dices
-yacht Sixes dices = sumOf 6 dices
-yacht FullHouse dices =
-  if isFullHouse
-    then total
-    else 0
+fullHouse :: [Int] -> Int
+fullHouse dices
+  | isFullHouse = sum dices
+  | otherwise = 0
   where
-    grouped = group $ sort dices
-    isFullHouse = length grouped == 2 && has2or3 (head grouped)
-    has2or3 xs = length xs == 2 || length xs == 3
-    total = sum dices
-yacht FourOfAKind dices = fourOfAKindSum
+    isFullHouse = (== [2, 3]) $ sort $ map length $ group $ sort dices
+
+fourOfAKind :: [Int] -> Int
+fourOfAKind dices = fourOfAKindSum
   where
-    grouped = group $ sort dices
-    fourKinds = maybe [] (take 4) $ find (\g -> length g >= 4) grouped
     fourOfAKindSum = sum fourKinds
-yacht LittleStraight dices =
-  if [1, 2, 3, 4, 5] == sort dices
-    then 30
-    else 0
-yacht BigStraight dices =
-  if [2, 3, 4, 5, 6] == sort dices
-    then 30
-    else 0
-yacht Choice dices = sum dices
-yacht Yacht dices =
-  if all (== head dices) (tail dices)
+    fourKinds = maybe [] (take 4) $ find (\g -> length g >= 4) grouped
+    grouped = group $ sort dices
+
+littleStraight :: [Int] -> Int
+littleStraight dices
+  | [1 .. 5] == sort dices = 30
+  | otherwise = 0
+
+bigStraight :: [Int] -> Int
+bigStraight dices
+  | [2 .. 6] == sort dices = 30
+  | otherwise = 0
+
+
+yacht':: [Int] -> Int
+yacht' dices =
+  if 1 == length (group dices)
     then 50
     else 0
+
+yacht :: Category -> [Int] -> Int
+yacht Ones           = sumOf 1
+yacht Twos           = sumOf 2
+yacht Threes         = sumOf 3
+yacht Fours          = sumOf 4
+yacht Fives          = sumOf 5
+yacht Sixes          = sumOf 6
+yacht FullHouse      = fullHouse
+yacht FourOfAKind    = fourOfAKind
+yacht LittleStraight = littleStraight
+yacht BigStraight    = bigStraight
+yacht Choice         = sum
+yacht Yacht          = yacht'
